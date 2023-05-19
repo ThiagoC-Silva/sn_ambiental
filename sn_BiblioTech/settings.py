dd6e54c6dcc1051b11874bb83843ca10230f81df
Thiago <thi89248@gmail.com>
2023-05-19 01:20:06 -0300

    Paginação

--- sn_BiblioTech/settings.py
+++ sn_BiblioTech/settings.py
@@ -132,3 +132,8 @@ REST_FRAMWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
