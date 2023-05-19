13409fcb571b8b6f004279b066c7e8ec11c15a0c
Thiago <thi89248@gmail.com>
2023-05-19 00:38:49 -0300

    Configuração de autenticação e autorização

--- gestao_BiblioTech/views.py
+++ gestao_BiblioTech/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
