1c0d7572cf1c5bbaaacf0715e5ecfa17b6e54dfb
Thiago Silva <thi89248@gmail.com>
2023-06-23 09:51:47 -0300

    create apps user and loan with yours routes

--- bibliotech/api_routers.py
+++ bibliotech/api_routers.py
@@ -2,8 +2,14 @@ from ninja import NinjaAPI
 
 #BOOKS
 from books.api import router as books_router
+from user.api import router as user_router
+from user.api import router as loan_router
 
 api = NinjaAPI()
 
 #BOOK ROUTE
 api.add_router('books/', books_router)
+#USER ROUTE
+api.add_router('user/', user_router)
+#LOAN ROUTE
+api.add_router('loan/', loan_router)
